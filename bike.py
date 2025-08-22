006afdd50a04059534e38016d1e950f5cf81ba42
Abdulraheem Nafisat <157598318+CodeWithNafisat@users.noreply.github.com>
2025-08-21 22:59:57 -0700

    bike.py

--- bike.py
+++ bike.py
@@ -45,7 +45,7 @@ if st.button('Submit'):
     st.success(f'Predicted number of bikes to rent: {int(prediction)}')
     
     # Load full dataset (for background reference)
-    data2 = pd.read_csv(r"C:\Users\Dell\Streamlit\Bike\sample.csv")
+    data2 = pd.read_csv(r"data\sample.csv")
     
     # Background dataset for SHAP (sample for speed)
     background_data = data2.sample(200, random_state=20)
